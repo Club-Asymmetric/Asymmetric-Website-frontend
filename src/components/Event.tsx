73a300bbac7933088ebc9e0361095e15fb6ccdb3
Linngeshwar <lingaeshrajan@gmail.com>
2024-11-14 09:31:21 +0530

    [fix] Dumb icon mistake fix (two lines removed :( )

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -37,12 +37,10 @@ export default function Event(
                     >{props.name}</p>
                     <div className="flex flex-row mt-[2rem]">
                         <div className="flex flex-row pr-[5rem]">
-                            <img src="/calendar.png" className="ml-[2rem] mr-[0.8rem] place-self-start" alt="date"></img>
                             <IoCalendarOutline className="ml-[2rem] mr-[1rem] place-self-start w-[22px] h-[22px]" />
                             <div className="text-white text-xl font-medium font-['Outfit'] leading-[25px]">{props.date}</div>
                         </div>
                         <div className="flex flex-row">
-                            <img src="/map_pin.png" className="ml-[2rem] mr-[0.8rem] place-self-start" alt="location"></img>
                             <RiMapPinLine className="ml-[2rem] mr-[1rem] place-self-start h-[22px] w-[22px]" />
                             <div className="text-white text-xl font-medium font-['Outfit'] leading-[25px]">{props.location}</div>
                         </div>
