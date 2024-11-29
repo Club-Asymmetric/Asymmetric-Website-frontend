8192f30e8313ece57756e2188c49a9a1a867dfef
HemanthRaj0C <personalhemanthraj@gmail.com>
2024-11-29 05:39:59 +0530

    [Fix] Responsive in Podcast

--- src/app/podcast/page.tsx
+++ src/app/podcast/page.tsx
@@ -49,35 +49,38 @@ const Podcast: React.FC<PropType> = ({ title, hosts, description, links, sourcef
     }, []);
 
     return (
-//w-[100%] my-10  bg-[#2b2e4a] grid grid-cols-[1fr_2fr] px-5 py-10 rounded-xl
-      // <div className='transition-transform transform hover:scale-105 duration-500 ease-in-out'>
-      <div className={`w-[100%] my-10  bg-gradient-to-br from-blue-950 via-blue-950 to-blue-800 grid grid-cols-[1fr_2fr] px-5 py-10 rounded-xl `}>
-            <div className="flex place-items-center">
-            <Image src={img} alt="Logo"  width={250} height={100} />
+      <div className={`w-[100%] my-10 bg-gradient-to-br from-blue-950 via-blue-950 to-blue-800 
+        grid grid-cols-1 lg:grid-cols-[1fr_2fr] px-5 py-10 rounded-xl`}>
+          <div className="flex justify-center lg:place-items-center mb-6 lg:mb-0">
+            <Image 
+            src={img} 
+            alt="Logo"  
+            width={250} 
+            height={100}
+            className="w-[200px] lg:w-[250px] hover:scale-105 transition-all duration-300 hover:animate-pulse" 
+            />
+          </div>
+          <div className="flex flex-col gap-4 px-2 lg:px-10">
+            <h1 className="text-2xl lg:text-3xl font-bold">{title}</h1>
+            <h2 className='font-imprintMTShadow text-sm lg:text-base'>{hostNames}</h2>
+            <p className="text-sm lg:text-base">
+            {description}
+            </p>
+            <h2 className='font-metal text-sm lg:text-base'>Editing by <span className='text-orange-500'>Asymmetric</span></h2>
+            <div className="icons flex flex-cols gap-4 items-center">
+            <Link href={spotifyLink} target='_blank' className="transition-colors">
+              <FaSpotify className="transition-all duration-300 w-4 lg:w-5 h-4 lg:h-5 hover:bg-green-400 rounded-lg" />
+            </Link>
+            <Link href={youTubeLink} target='_blank' className="transition-colors">
+              <FaYoutube className="transition-all duration-300 w-4 lg:w-5 h-4 lg:h-5 hover:bg-red-600 rounded-lg" />
+            </Link>
+            <Link href={appleLink} target='_blank' className="transition-colors">
+              <FaApple className="transition-all duration-300 w-4 lg:w-5 h-4 lg:h-5 hover:bg-gray-400 rounded-lg" />
+            </Link>
             </div>
-            <div className="flex flex-col gap-4 px-10">
-              <h1 className="text-3xl font-bold">{title}</h1>
-              <h2 className='font-imprintMTShadow'>{hostNames}</h2>
-              <p>
-                {description}
-              </p>
-              <h2 className='font-metal'>Editing by <span className='text-orange-500'>Asymmetric</span></h2>
-              <div className="icons flex flex-cols gap-4 items-center">
-                <Link href={spotifyLink} target='_blank' className="transition-colors">
-                  <FaSpotify className="transition-all duration-300 w-5 h-5 hover:bg-green-400 rounded-lg" />
-                </Link>
-                <Link href={youTubeLink} target='_blank' className="transition-colors">
-                  <FaYoutube className="transition-all duration-300 w-5 h-5 hover:bg-red-600 rounded-lg" />
-                </Link>
-                <Link href={appleLink} target='_blank' className="transition-colors">
-                  <FaApple className="transition-all duration-300 w-5 h-5 hover:bg-gray-400 rounded-lg" />
-                </Link>
-
-              </div>
-              <MusicPlayer sourceFile={sourcefile} />
-            </div>
-      </div>
-      // </div>
+            <MusicPlayer sourceFile={sourcefile} />
+          </div>
+        </div>
     );
   }
 
@@ -87,11 +90,13 @@ export default function Podcasts(){
   const hosts = ['host1', 'host2'];
   const links = ['https://open.spotify.com/show/0iMKRNbZOWxKWIAUYD7T0C', 'https://open.spotify.com/show/0iMKRNbZOWxKWIAUYD7T0C', 'https://open.spotify.com/show/0iMKRNbZOWxKWIAUYD7T0C'];
   const description = 'soluta, explicabo, quam assumenda facilis. Tempore dolor tempora nisi minus inventore explicabo! Sunt facere inventore qui ex molestiae nostrum nulla eligendi omnis minima nam dolorem officia, cupiditate harum aliquam reprehenderit porro officiis ab blanditiis! Accusamus facilis aspernatur, Consequuntur, modi tenetur quo a impedit animi sapiente excepturi ipsum optio rem consequatur sint fugit? Facere, dolor. Adipisci voluptates similique, a minus voluptas excepturi? Molestias beatae, nulla fuga voluptatum aliquid harum maxime nam. Odit necessitatibus dolorem saepe dicta';
-  return <div className='w-[80%] mx-auto'>
-
-    <h1 className="text-3xl font-bold">Podcasts</h1>
+  
+  return (
+  <div className='w-[95%] lg:w-[80%] mx-auto'>
+    <h1 className="text-2xl lg:text-3xl font-bold">
+      Podcasts
+    </h1>
     <Podcast title='Asymmetric Podcast' hosts={hosts} links={links} description={description} sourcefile='/song.mp3' img='/ass.png' />
     <Podcast title='Asymmetric Podcast' hosts={hosts} links={links} description={description} sourcefile='/song.mp3' img='/ass.png' />
-
-  </div>
+  </div>)
 };
