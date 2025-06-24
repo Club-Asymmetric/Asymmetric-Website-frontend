ba3e7cc0a539450e8cd232051bbba9172b1b43f4
HemanthRaj0C <personalhemanthraj@gmail.com>
2025-06-24 15:06:59 +0530

    [Add] FaviConn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,7 +77,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`h-full ${outfit.variable} ${oswald.variable} ${aBeeZee.variable} ${nicoMoji.variable} ${parisienne.variable} ${metal.variable} ${imprintMTShadow.variable} ${geistSans.variable}`}>
       <head>
-
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         className={`antialiased min-h-screen flex flex-col relative font-outfit`}
